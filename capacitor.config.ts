300b03c9d023fe0c26fe6d83952f804c4748eaa3
Pierre André Valles <196116350+Mercedes1986@users.noreply.github.com>
2026-03-15 18:28:05 +0100

    Mise a jours

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.batiquant.app',
+  appName: 'BatiQuant',
+  webDir: 'dist'
+};
+
+export default config;
